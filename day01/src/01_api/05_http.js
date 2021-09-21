a3e9c0d64ba880c5d94495b5e407379b35aabe3d
BellaZZZZZ <bellazhao_321@qq.com>
2021-09-21 09:12:37 +0800

    fix: 补全

--- day01/src/01_api/05_http.js
+++ day01/src/01_api/05_http.js
@@ -2,9 +2,9 @@ const fs = require('fs')
 const http = require('http')
 
 http.createServer((request, response) => {
-  // console.log('a request')
+  //console.log('a request', getPrototypeChain(response))
   // response.end('Hi Node')
-  const {url, method} = request
+  const {url, method, headers} = request
   if(url === '/' && method === 'GET') {
     fs.readFile('./index.html', (err, data) => {
       if(err) {
@@ -20,7 +20,13 @@ http.createServer((request, response) => {
       response.end(data)
     })
   }
-  //else if(url ===)
+  else if(url === '/users' && method === 'GET') {
+    response.writeHead(200, {'Content-Type':'application/json'})
+    response.end(JSON.stringify({name:'tom'}))
+  }
+  else if(method === 'GET' && headers.accept.indexOf('image/*') !== -1){
+    
+  }
   else {
     response.statusCode = 400
     response.setHeader('Content-Type','text/plain;charset=utf-8')
@@ -31,5 +37,9 @@ http.createServer((request, response) => {
 })
 
 function getPrototypeChain(obj) {
-
+  const protoChain = []
+  while(obj = Object.getPrototypeOf(obj)){
+    protoChain.push(obj)
+  }
+  return protoChain
 }
